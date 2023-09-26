7d73fb6fe433a0473df5620a383bf2824bbc4b5a
chenjunyu <chenjunyu8@tal.com>
2023-09-26 11:54:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+运行：
+1. pip install -r requirements.txt
+2. sh run.sh
---
+++ yolov5/run.sh
@@ -0,0 +1,1 @@
+python detect.py --weights runs/train/row_col/weights/best.pt --source ../images/border_0_0AUFQKYOR2PMM5IQJGBK.jpg
